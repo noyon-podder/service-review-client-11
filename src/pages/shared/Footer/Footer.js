b7ebfc4ad02b2a6578ba9d927ac39def77d69a5f
noyon-podder <noyon.vai369@gmail.com>
2022-11-08 01:38:37 +0600

    navbar and footer add this commmit

--- src/pages/shared/Footer/Footer.js
+++ src/pages/shared/Footer/Footer.js
@@ -1,11 +1,36 @@
-import React from 'react';
-
+import React from "react";
+import logo from "../../../images/logo-icon.png"
 const Footer = () => {
-    return (
-        <div>
-            <h3>here is footer</h3>
+  return (
+    <div className="bg-black ">
+      <footer className="footer p-10  text-base-content max-w-6xl mx-auto">
+        <div className="text-center">
+          <img src={logo} alt="Footer Logo" className="w-15 mx-auto" />
+          <span className="font-semibold text-white mt-7"> Copyright 2019 © Developed by ThemeLooks.</span>
         </div>
-    );
+        <div className="text-white">
+          <span className="footer-title">Services</span>
+          <a href className="link link-hover">Corporate Law</a>
+          <a href className="link link-hover">Real state law</a>
+          <a href className="link link-hover">Financial law</a>
+          <a href className="link link-hover">Family law</a>
+        </div>
+        <div className="text-white">
+          <span className="footer-title">Details</span>
+          <a href className="link link-hover">Reviews</a>
+          <a href className="link link-hover">About us</a>
+          <a href className="link link-hover">Contact</a>
+          <a href className="link link-hover">Share</a>
+        </div>
+        <div className="text-white">
+          <span className="footer-title">Legal</span>
+          <a href className="link link-hover">Terms of use</a>
+          <a href className="link link-hover">Privacy policy</a>
+          <a href className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/shared/Navbar/Navbar.js
+++ src/pages/shared/Navbar/Navbar.js
@@ -1,10 +1,34 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logo-icon.png';
 const Navbar = () => {
     return (
-        <div>
-            <h3>here is navbar</h3>
+        <header className='bg-black'>
+            <div className="navbar max-w-6xl mx-auto py-5">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <Link><li className='text-black font-3xl font-semibold'>Home</li></Link>
+          <Link><li className='text-black font-3xl font-semibold'>Services</li></Link>
+            </ul>
+          </div>
+          <Link className="normal-case text-xl flex items-center"><img src={logo} alt="" className='w-12 mr-3' /> <span className='font-bold text-3xl text-white uppercase '>Justice</span></Link>
         </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">
+          <Link><li className='text-white font-3xl font-semibold mr-5 hover:text-orange-600'>Home</li></Link>
+          <Link><li className='text-white font-3xl font-semibold mr-5 hover:text-orange-600'>Services</li></Link>
+              
+          </ul>
+        </div>
+        <div className="navbar-end">
+        <Link> <button className='btn btn-warning px-6 text-black font-semibold'>Register</button></Link>
+        </div>
+      </div>
+        </header>
     );
 };
 
